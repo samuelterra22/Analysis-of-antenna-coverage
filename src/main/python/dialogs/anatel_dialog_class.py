fd898866a7456f5498befe1cacdc887571df24cf
Samuel Terra <samuelterra22@hotmail.com>
2019-10-11 15:14:30 -0300

    Implement simple example for progress bar using threads

--- src/main/python/dialogs/anatel_dialog_class.py
+++ src/main/python/dialogs/anatel_dialog_class.py
@@ -1,5 +1,9 @@
+import time
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog
+
+import threading
 
 AnatelQDialog = uic.loadUiType("./views/anatel_dialog.ui")[0]
 
@@ -8,3 +12,15 @@ class AnatelDialogClass(QDialog, AnatelQDialog):
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
+
+        self.trigger_fill_progress_bar_thread()
+
+    def trigger_fill_progress_bar_thread(self):
+        x = threading.Thread(target=self.fill_progress_bar)
+        x.start()
+
+    def fill_progress_bar(self):
+        for i in range(101):
+            self.progress_bar_anatel.setValue(i)
+            self.progress_bar_anatel.setLabel("aasdasd")
+            time.sleep(0.1)

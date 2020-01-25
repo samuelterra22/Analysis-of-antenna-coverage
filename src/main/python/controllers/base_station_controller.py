7f16a0e88829632c5ed729871e91d98b4c7df86b
Samuel Terra <samuelterra22@hotmail.com>
2020-01-25 16:38:43 -0300

    Implement simpre base station controller and simple base station service

--- src/main/python/controllers/base_station_controller.py
+++ src/main/python/controllers/base_station_controller.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+from src.main.python.controllers.base_controller import BaseController
 
-class BaseStationController():
-    pass
+
+class BaseStationController(BaseController):
+    def index(self):
+        pass
+
+    def add(self):
+        pass
+
+    def show(self):
+        pass
+
+    def edit(self):
+        pass
+
+    def update(self):
+        pass
+
+    def destroy(self):
+        pass
--- src/main/python/services/base_station_service.py
+++ src/main/python/services/base_station_service.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
+from src.main.python.repositories.base_station_repository import BaseStationRepository
 
-class BaseStationService():
-    pass
+
+class BaseStationService:
+
+    def __init__(self):
+        self.__repository = BaseStationRepository()
+
+    def store(self, data):
+        try:
+            self.__repository.store(data)
+        except Exception as e:
+            print(e)
+
+    def update(self, data, id):
+        self.__repository.find_one_by_id(id)
+
+        try:
+            self.__repository.update(data, id)
+        except Exception as e:
+            print(e)
+
+    def destroy(self, id):
+        self.__repository.find_one_by_id(id)
+
+        try:
+            self.__repository.delete(id)
+        except Exception as e:
+            print(e)

25ecc6ecae5d13ebdb8ee0d630f7a0b1596c7181
Samuel Terra <samuelterra22@hotmail.com>
2020-01-25 13:07:37 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Analysis-of-antenna-coverage',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/samuelterra22/Analysis-of-antenna-coverage',
+    license='GNU General Public License v3.0',
+    author='Samuel Terra',
+    author_email='samuelterra22@gmail.com',
+    description='A software for simulating the radio frequency spectrum transmitting voice to base stations.'
+)

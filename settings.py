0a26a6ff945164b648b8b61f2fb1351dde67e6a6
Samuel Terra <samuelterra22@hotmail.com>
2020-02-11 22:30:24 -0300

    Add python-dotenv and load environment variables

---
+++ settings.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+ENV_FILE = os.path.join(BASE_DIR, ".env")
+load_dotenv(dotenv_path=ENV_FILE)
+
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+
+def get_env_keys():
+    """
+    This function read the .env file and get all env keys from the file
+    :return: list Return the .env keys
+    """
+    env_vars = []
+    with open(ENV_FILE) as f:
+        for line in f:
+            if not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                env_vars.append(key)
+
+    return env_vars

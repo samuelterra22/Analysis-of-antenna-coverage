c3ed50efce7da0245aa430b021b89361606bf3de
Samuel Terra <samuelterra22@hotmail.com>
2020-02-16 17:20:27 -0300

    Insert and get uf and county ids in comboboxes

--- src/main/python/dialogs/settings_dialog_class.py
+++ src/main/python/dialogs/settings_dialog_class.py
@@ -3,7 +3,7 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog, QComboBox
 
-from src.main.python.utils.region import get_ufs_initials, get_counties
+from src.main.python.utils.region import get_ufs_initials, get_counties, get_uf_code, get_uf_by_id
 
 SettingsQDialog = uic.loadUiType("./views/settings_dialog.ui")[0]
 
@@ -12,6 +12,7 @@ class SettingsDialogClass(QDialog, SettingsQDialog):
     """
     This class load the settings dialog pyqt component
     """
+
     def __init__(self, parent=None):
         """
         Settings dialog class constructor
@@ -22,28 +23,29 @@ class SettingsDialogClass(QDialog, SettingsQDialog):
 
         self.fill_combo_box_country_state()
 
-        self.combo_box_country_state.currentTextChanged.connect(self.on_combo_box_country_state_changed)
+        self.combo_box_country_state.currentIndexChanged.connect(self.on_combo_box_country_state_changed)
 
-        self.combo_box_country_county.currentTextChanged.connect(self.on_combo_box_country_county_changed)
+        self.combo_box_country_county.currentIndexChanged.connect(self.on_combo_box_country_county_changed)
 
-    def on_combo_box_country_state_changed(self, value):
+    def on_combo_box_country_state_changed(self, index):
         """
         This method is fired when combo box country state is changed
         :param value:
         :return:
         """
         self.combo_box_country_county.clear()
-        if value is not "Select":
-            self.fill_combo_box_country_county(value)
+        if index is not 0:
+            uf_id = self.combo_box_country_state.itemData(index)
+            uf_initial = get_uf_by_id(uf_id)
+            self.fill_combo_box_country_county(uf_initial)
 
-    def on_combo_box_country_county_changed(self, value):
+    def on_combo_box_country_county_changed(self, index):
         """
         This method is fired when combo box county state is changed
-        :param value:
-        :return:
+        :param int index: Index of county in combo_box_country_county component
+        :return: None
         """
-        # Do something
-        pass
+        county_id = self.combo_box_country_county.itemData(index)
 
     def fill_combo_box_country_county(self, uf):
         """
@@ -54,7 +56,7 @@ class SettingsDialogClass(QDialog, SettingsQDialog):
         self.combo_box_country_county: QComboBox
         counties = get_counties(uf)
         for county in counties:
-            self.combo_box_country_county.addItem(county[0])
+            self.combo_box_country_county.addItem(county[0], county[1])
 
     def fill_combo_box_country_state(self):
         """
@@ -66,4 +68,4 @@ class SettingsDialogClass(QDialog, SettingsQDialog):
         self.combo_box_country_state.addItem("Select", -1)
 
         for uf in ufs:
-            self.combo_box_country_state.addItem(uf)
+            self.combo_box_country_state.addItem(uf, get_uf_code(uf))

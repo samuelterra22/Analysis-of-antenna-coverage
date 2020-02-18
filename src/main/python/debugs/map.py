946fe6c07c8f93df9b4bba7d99e1141aee559745
Samuel Terra <samuelterra22@hotmail.com>
2020-02-17 23:19:53 -0300

    Add pyqtlet

---
+++ src/main/python/debugs/map.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
+from pyqtlet import L, MapWidget
+
+
+# https://github.com/skylarkdrones/pyqtlet
+
+class MapWindow(QWidget):
+    def __init__(self):
+        # Setting up the widgets and layout
+        super().__init__()
+        self.mapWidget = MapWidget()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.mapWidget)
+        self.setLayout(self.layout)
+
+        # Working with the maps with pyqtlet
+        self.map = L.map(self.mapWidget)
+        self.map.setView([12.97, 77.59], 10)
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(self.map)
+        self.marker = L.marker([12.934056, 77.610029])
+        self.marker.bindPopup('Maps are a treasure.')
+        self.map.addLayer(self.marker)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = MapWindow()
+    sys.exit(app.exec_())

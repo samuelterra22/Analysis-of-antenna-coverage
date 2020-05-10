c3250cc36979f600506455b938037306d2275f35
Samuel Terra <samuelterra22@hotmail.com>
2020-05-10 19:42:54 -0300

    Add examples for maps

---
+++ src/main/python/utils/color.py
@@ -0,0 +1,71 @@
+from colour import Color
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+def get_color_gradient(steps=250):
+    cores = list(Color("red").range_to(Color("green"), steps))
+    cores.pop(0)
+    cores.pop(len(cores) - 1)
+    return cores
+
+
+def get_color_of_interval(x, max_value=-30, min_value=-100):
+    """
+    Este método retorna uma cor de acordo com o valor que está entre o intervalo min-max. Em outras palavras,
+    este método transforma um número em uma cor dentro de uma faixa informada.
+    :param min_value: Valor mínimo do intervalo.
+    :param max_value: Valor máximo do intervalo.
+    :param x: Valor que está dentro do intervalo e que deseja saber sua cor.
+    :return: Retorna uma tupla representando um cor no formato RGB.
+    """
+
+    if PAINT_BLACK_BELOW_SENSITIVITY and x < SENSITIVITY:
+        return BLACK
+
+    percentage = get_percentage_of_range(min_value, max_value, x)
+    color = get_value_in_list(percentage, COLORS)
+
+    return color
+
+
+def get_percentage_of_range(min_value, max_value, x):
+    """
+    Método responsável por retornar a porcentagem de acordo com um respectivo intervalo.
+    :param min_value: Valor mínimo do intervalo.
+    :param max_value: Valor máximo do intervalo.
+    :param x: Valor que está no intervalo de min-max que deseja saber sua respectiva porcentagem.
+    :return: Retorna uma porcentagem que está de acordo com o intervalo min-max.
+    """
+
+    return ((x - min_value) / (max_value - min_value)) * 100
+
+
+def get_value_in_list(percent, list_numbers):
+    """
+    Método retorna o valor de uma posição de uma lista. A posição é calculada de acordo a porcentagem.
+    :param percent: Valor float representando a porcentagem.
+    :param list_numbers: Lista com n números.
+    :return: Retorna a cor da posição calculada.
+    """
+    position = (percent / 100) * len(list_numbers)
+    if position < 1:
+        position = 1
+    elif position >= len(list_numbers):
+        position = len(list_numbers)
+    return hex_to_rgb(list_numbers[int(position - 1)])
+
+
+def hex_to_rgb(hex_value):
+    """
+    Método responsável por converter uma cor no formato hexadecial para um RGB.
+    :param hex_value: Valor em hexadecimal da cor.
+    :return: Tupla representando a cor em formato RGB.
+    """
+    hex_value = str(hex_value).lstrip('#')
+    lv = len(hex_value)
+    return tuple(int(hex_value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
--- src/main/python/windows/main_window.py
+++ src/main/python/windows/main_window.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import random
 
 from PyQt5 import uic
 from pyqtlet import L, MapWidget
@@ -25,11 +26,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         Main window constructor
         :param parent:
         """
-        QMainWindow.__init__(self, parent)
+        # Init UI
+        super().__init__()
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-        self.init_coverage_map()
+        self._init_map()
         # self.init_mos_map()
 
         # Calculate button
@@ -44,26 +46,85 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.menu_action_about.triggered.connect(self.on_menu_about_triggered)
         self.menu_action_help.triggered.connect(self.on_menu_help_triggered)
 
-    def init_coverage_map(self):
-        self.coverage_map_widget = MapWidget()
-        self.vertical_layout_coverage_map.addWidget(self.coverage_map_widget)
+    def generate_random_data(self, lat, lon):
+        dec_lat = (random.random() / 100) * random.choice([-1, 1])
+        dec_lon = (random.random() / 100) * random.choice([-1, 1])
+        return [round(lat + dec_lat, 15), round(lon + dec_lon, 15)]
 
-        # Working with the maps with pyqtlet
-        self.coverage_map = L.map(self.coverage_map_widget)
-        self.coverage_map.setView([-21.2284575, -44.9753476], 16)
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(self.coverage_map)
+    def set_zoom_warning(self, zoom):
+        if zoom < 6:
+            print('Woah buddy. You\'re flying \
+                pretty high. Hope you don\'t have vertigo..')
+        else:
+            print('Yup, no worries. A fall from \
+                here shoudn\'t hurt... too much.')
 
-        self.coverage_marker = L.marker([-21.2284575, -44.9753476])
-        self.coverage_marker.bindPopup('Maps are a treasure.')
-        self.coverage_map.addLayer(self.coverage_marker)
+    def set_warnings(self):
+        self.map.getZoom(self.set_zoom_warning)
 
-        self.coverage_marker = L.circleMarker([-21.227026, -44.9778903], {
+    def _get_marker_radius(self, value):
+        # We want the marker size to be exponential to the number
+        power = (1 / 3)
+        MAX_SIZE = 1  # ?
+        return MAX_SIZE * (value ** power) / (self.highIncidents ** power)
+
+    def on_map_clicked(self, event):
+        print(event)
+
+    def on_map_zoomed(self, event):
+        print(event)
+
+    def _init_map(self):
+        self.map_widget = MapWidget()
+        self.vertical_layout_coverage_map.addWidget(self.map_widget)
+
+        self.map = L.map(self.map_widget)
+        # {
+        #     'minOpacity': 0.05,
+        #     'maxZoom': 18,
+        #     'radius': 25,
+        #     'blur': 15,
+        #     'max': 1.0
+        # }
+        self.map.setView([-21.2284575, -44.9753476], 16)
+
+        # self.map.clicked.connect(self.on_map_clicked)
+        # self.map.zoom.connect(self.on_map_zoomed)
+
+        # self.map.getBounds(lambda bounds: print(bounds))
+
+        # self.set_warnings()
+
+        # L.tileLayer('http://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png').addTo(self.map)
+        # L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {'noWrap': 'true'}).addTo(self.map)
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(self.map)
+
+        for _ in range(50):
+            coo = self.generate_random_data(-21.227026, -44.9778903)
+            self.marker = L.circleMarker(coo, {
+                'color': "red",
+                'opacity': 0.5,
+                # 'fillOpacity': 0.9,
+                # 'stroke': 1,
+                # 'weight': 10,
+                # 'lineCap': 'round',
+                # 'fillColor': "lightgreen",
+                # 'radius': 25,  # self._get_marker_radius(0)
+            })
+
+            self.marker.bindPopup('coo')
+            self.map.addLayer(self.marker)
+
+        self.marker = L.circleMarker([-21.227026, -44.9778903], {
             'color': '#a31286',
             'fillOpacity': 0.3,
             'stroke': 0,
             'weight': 3
         })
-        self.coverage_map.addLayer(self.coverage_marker)
+
+        self.map.clicked.connect(lambda x: print(x))
+
+        self.map.addLayer(self.marker)
 
     @pyqtSlot(name="on_button_calculate_clicked")
     def on_button_calculate_clicked(self):

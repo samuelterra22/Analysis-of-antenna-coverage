8c90fce9e262aa31a7e540661f67b389032a8f0e
Samuel Terra <samuelterra22@hotmail.com>
2020-05-21 09:19:17 -0300

    Fix example

--- src/main/python/debugs/f.py
+++ src/main/python/debugs/f.py
@@ -3,27 +3,38 @@ import matplotlib
 import numpy as np
 
 
-def sample_data(shape=(73, 145)):
-    nlats, nlons = shape
+def generate_propagation_map_layer(shape_of_propagation, lat_bounds, long_bounds, ):
+    n_lats, n_lons = shape_of_propagation
 
-    lats = np.linspace(np.deg2rad(-21.211645), np.deg2rad(-21.246091), nlats)
-    lons = np.linspace(np.deg2rad(-44.995876), np.deg2rad(-44.954157), nlons)
+    lats = np.linspace(np.deg2rad(lat_bounds[0]), np.deg2rad(lat_bounds[1]), n_lats)
+    lons = np.linspace(np.deg2rad(long_bounds[0]), np.deg2rad(long_bounds[1]), n_lons)
 
     lons, lats = np.meshgrid(lons, lats)
 
     lats = np.rad2deg(lats)
     lons = np.rad2deg(lons)
-    data = np.random.randint(-200, -60, size=(nlats, nlons))
+
+    count = 1
+    data = np.empty([n_lats, n_lons])
+    for i in range(n_lons):
+        for j in range(n_lats):
+            data[i][j] = count
+            count += 1
+
+    print(data.shape)
 
     return lons, lats, data
 
 
-# -21.2262173,-44.9779183
-
-lon, lat, data = sample_data(shape=(1000, 1000))
-# lon -= 180
+lon, lat, data = generate_propagation_map_layer(
+    lat_bounds=(-21.211645, -21.246091),
+    long_bounds=(-44.995876, -44.954157),
+    shape_of_propagation=(1000, 1000)
+)
 
 color_map = matplotlib.cm.get_cmap('rainbow')
+
+print(type(data))
 
 normed_data = (data - data.min()) / (data.max() - data.min())
 colored_data = color_map(normed_data)
